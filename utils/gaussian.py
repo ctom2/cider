32f83f8b2d2d985e21ceea360cf4d0fec093ca12
Chobola <chobola@in.tum.de>
2023-07-30 11:47:38 +0200

    additions

---
+++ utils/gaussian.py
@@ -0,0 +1,13 @@
+import numpy as np
+from scipy import signal
+
+def gaussian_kernel(n, std, normalised=False):
+    '''
+    Generates a n x n matrix with a centered gaussian
+    of standard deviation std centered on it. If normalised,
+    its volume equals 1.'''
+    gaussian1D = signal.gaussian(n, std)
+    gaussian2D = np.outer(gaussian1D, gaussian1D)
+    if normalised:
+        gaussian2D /= (2*np.pi*(std**2))
+    return gaussian2D
